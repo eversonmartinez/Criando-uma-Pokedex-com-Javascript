4bf1394f389a7d04da49403efe54489a5fb5addb
Everson Martinez <118080010+eversonmartinez@users.noreply.github.com>
2023-09-13 15:29:41 -0300

    Remoção de códigos sem uso

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,42 +1,8 @@
 
 const pokeApi = {}
 
-// async function getSpeciesInfoFromPokeDetail(urlSpecies) {
-//     const response = await fetch(urlSpecies);
-//     const jsonBody = await response.json();
-//     return jsonBody;
-//   }
-  
-//   async function addMoreDetailsToPokemon(pokemon, pokeDetail) {
-//     try {
-//       const specieInfo = await getSpeciesInfoFromPokeDetail(pokeDetail.species.url);
-  
-//       console.log(specieInfo.id);
-//       pokemon.habitat = specieInfo.habitat.name;
-//       pokemon.height = pokeDetail.height;
-//       pokemon.weight = pokeDetail.weight;
-//       pokemon.abilities = pokeDetail.abilities.map((abilitySlot) => abilitySlot.ability.name);
-  
-//       return pokemon;
-//     } catch (error) {
-//       console.error('Erro ao obter informações da espécie:', error);
-//     }
-//   }
-
-function getSpeciesInfoFromPokeDetail(urlSpecies){
-    specie = fetch(urlSpecies).then((response) => response.json()).then((jsonBody) => jsonBody)
-    return(specie)
-}
 
 function addMoreDetailsToPokemon(pokemon, pokeDetail){
-    //const specieInfo = getSpeciesInfoFromPokeDetail(pokeDetail.species.url)
-    // () => fetch(pokeDetail.species.url)
-    // .then((response) => response.json())
-    // .then((detailRequests) => Promise.all(detailRequests)) 
-    // .then((pokemonsDetails) => pokemonsDetails) 
-    
-    //pokemon.eggGroups = specieInfo.egg_groups.map((eggSlot) => eggSlot.name)
-    //console.log(specieInfo.id)
     pokemon.height = pokeDetail.height
     pokemon.weight = pokeDetail.weight
     pokemon.abilities = pokeDetail.abilities.map((abilitySlot) => abilitySlot.ability.name)
@@ -46,22 +12,13 @@ function addMoreDetailsToPokemon(pokemon, pokeDetail){
         pokemon.habitat=specieInfo.habitat.name
         pokemon.eggGroups = specieInfo.egg_groups.map((eggSlot) => eggSlot.name)
         return pokemon})
-    //pokemon.habitat = specieInfo.habitat.name
-    // pokemon.height = pokeDetail.height
-    // pokemon.weight = pokeDetail.weight
-    //
-    
-    //se der algum problema é pq a função esta retornando o pokemon antes de terminar os fetchs logo a cima
-    //return pokemon
+
 }
 
 function convertPokeApiDetailToPokemon(pokeDetail){
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
-    
-    // pokemon.types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    // pokemon.type = pokemon.types.get(0)
 
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
@@ -71,23 +28,7 @@ function convertPokeApiDetailToPokemon(pokeDetail){
 
     pokemon.photo = pokeDetail.sprites.other['official-artwork'].front_default
 
-    // pokemon.height = pokeDetail.height
-    // pokemon.weight = pokeDetail.weight
-    // pokemon.abilities = pokeDetail.abilities.map((abilitySlot) => abilitySlot.ability.name)
-    
-    // const specieInfo = getSpeciesInfoFromPokeDetail(pokeDetail.species.url).then(pokemon.eggGroups = specieInfo.egg_groups.map((eggSlot) => eggSlot.name))
-    
-    
-    // pokemon.habitat = specieInfo.habitat.name
-
     return pokemon
-}
-
-pokeApi.getPokemonDetailed = (pokemon) => {
-    return fetch(pokemon.url)
-            .then((response) => response.json())
-            .then(convertPokeApiDetailToPokemon)
-            .then((pokemonSimple) => addMoreDetailsToPokemon(pokemonSimple, pokemon))
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
@@ -114,20 +55,7 @@ pokeApi.getPokemon = (pokemonId = 1) => {
     .then((response) => response.json())
     .then((pokemon) => {
         detailedPokemon = convertPokeApiDetailToPokemon(pokemon)
-        //detailedPokemon = pokeApi.getPokemonDetailed(pokemon)
         detailedPokemon = addMoreDetailsToPokemon(detailedPokemon, pokemon)
         return detailedPokemon
     })
-    // .then((detailRequests) => Promise.all(detailRequests))  
-    // .then((pokemonsDetails) => {debugger 
-    //     return pokemonsDetails})
 }
-
-// Promise.all([
-//     fetch('https://pokeapi.co/api/v2/pokemon/1'),
-//     fetch('https://pokeapi.co/api/v2/pokemon/2'),
-//     fetch('https://pokeapi.co/api/v2/pokemon/3'),
-//     fetch('https://pokeapi.co/api/v2/pokemon/4')
-// ]).then((results) => {
-//     console.log(results)
-// })
